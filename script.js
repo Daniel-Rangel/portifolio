673193d9b5d9931442299d48aacc753d8fc12c9a
Daniel Rangel <danf.arangel@gmail.com>
2022-03-27 18:19:27 -0300

    feito seção sobre min

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ imagens.forEach((elementoImagens , indexImagens) => {
     console.log( calc , indexImagens)
 
     elementoImagens.style.width = `${  indexImagens >= 29 ? 43 : calc + 5}%`
-    elementoImagens.style.top = `${indexImagens >= 29 ? 47: indexImagens + 19 }%`
+    elementoImagens.style.top = `${indexImagens >= 29 ? 47 : indexImagens + 19 }%`
     elementoImagens.style.right = `${ imagens.length - (indexImagens + 25)}%`
     elementoImagens.style.animationDelay = `${(indexImagens * 60)}ms`
     elementoImagens.style.opacity = `${indexImagens >= 10 ? 1 : (`0.${indexImagens}`) }`
