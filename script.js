8f8ac9c5a2eec878cc456c721d4cfb21a37f8d11
Daniel Rangel <danf.arangel@gmail.com>
2022-03-31 21:56:28 -0300

    experience feito

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 const data = [
     {
+        id: 1,
         empresa: 'Digital House',
         profissao: 'Professor conteudista em Frontend',
-        data:'Nov 2021 - Atual',
+        data:'Nov 2018 - Atual',
         descrição: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'
     },
     {
+        id: 2,
         empresa: 'Código Fonte TV',
-        profissao: 'Professor conteudista em Frontend',
-        data:'Nov 2021 - Atual',
+        profissao: 'Condutor',
+        data:'Jun 2015 - 2018',
         descrição: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'
     },
     {
+        id: 3,
         empresa: 'Zuplae',
-        profissao: 'Professor conteudista em Frontend',
-        data:'Nov 2021 - Atual',
+        profissao: 'Front-end',
+        data:'Abr 2012 - 2015',
         descrição: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'
     },
     {
+        id: 4,
         empresa: 'ContWeb',
-        profissao: 'Professor conteudista em Frontend',
-        data:'Nov 2021 - Atual',
+        profissao: 'Calculador',
+        data:'Mar 2010 - 2012',
         descrição: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'
     },
     {
+        id: 5,
         empresa: 'iuricode',
-        profissao: 'Professor conteudista em Frontend',
-        data:'Nov 2021 - Atual',
+        profissao: 'Separador',
+        data:'Dez 2009 - 2010',
         descrição: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'
     },
     {
+        id: 6,
         empresa: 'Freelancer',
-        profissao: 'Professor conteudista em Frontend',
-        data:'Nov 2021 - Atual',
+        profissao: 'Juntador',
+        data:'Nov 2007 - 2008',
         descrição: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'
     }
 ]
@@ -55,13 +61,45 @@ imagens.forEach((elementoImagens , indexImagens) => {
 data.map( empresa => {
     let li = document.createElement('li')
     let text = document.createTextNode(empresa.empresa)
+
+    const compH3 = document.querySelector('.comp_description h3')
+    const compSpan = document.querySelector('.comp_description span')
+    const companiesSpan = document.querySelector('.companies_profession')
+    const companiesP = document.querySelector('.companies_description p')
     
     li.appendChild(text)
     experience_companies.appendChild(li)
 
+    //faz verificação e permite o primeiro objeto ser adicionado
+    if(empresa.id === 1 && li.className === ''){
+        li.classList.add('ativo')
+        
+        companiesSpan.innerText = empresa.profissao
+        companiesP.innerText = empresa.descrição
+        compH3.innerText = empresa.empresa
+        compSpan.innerText = empresa.data
+    }
+
+    //1° cria uma lista e verifica se tem uma classe "ativo" para ser removido,
+    //2° adiciona uma classe "ativo" no elemento clicado;
+    //3° preenche as informação de acordo o objeto;
     li.addEventListener('click' , (i)=>{
+        let lists = document.querySelectorAll('.experience_companies ul li')
+        
+        for(let list of lists){
+            list.classList.remove('ativo')
+        }
+
         i.target.classList.toggle('ativo')
-        console.log(i.target.innerHTML)
+        
+        if(i.target.innerText === empresa.empresa){
+
+            companiesSpan.innerText = empresa.profissao
+            companiesP.innerText = empresa.descrição
+            compH3.innerText = empresa.empresa
+            compSpan.innerText = empresa.data
+        }
+        console.log(li.className)
     })
 })
 
